035b6a5d40427e3fbc6cebd49ae42d8ba4f7646f
Peranikov <ym.nekoze@gmail.com>
2017-05-06 22:39:52 +0900

    games.title null: false

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170501091412) do
 
   create_table "games", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
