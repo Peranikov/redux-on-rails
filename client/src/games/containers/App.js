ebc7da4e7c758aee1e9413761e6db054f37802ae
Peranikov <ym.nekoze@gmail.com>
2017-05-06 21:57:55 +0900

    Redesign

--- client/src/games/containers/App.js
+++ client/src/games/containers/App.js
@@ -23,8 +23,8 @@ class App  extends Component {
 
     return(
       <div>
-        <GameList games={games} />
         <Form onSubmit={this.handleSubmit}/>
+        <GameList games={games} />
       </div>
     )
   }
