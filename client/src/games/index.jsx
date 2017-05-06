d13a1d0b3b77baa9c5b1973b205edbbbad61df26
Peranikov <ym.nekoze@gmail.com>
2017-05-06 11:14:04 +0900

    Add redux-logger

--- client/src/games/index.jsx
+++ client/src/games/index.jsx
@@ -1,18 +1,9 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
-import App from './containers/App'
-import reducer from './reducers'
-import { createLogger } from 'redux-logger'
-import { Provider } from 'react-redux'
-import configureStore from './store/configureStore'
-import rootSaga from './sagas'
-
-// const logger = createLogger();
-// const store = createStore(
-//   reducer,
-//   applyMiddleware(logger)
-// );
+import React from "react";
+import {render} from "react-dom";
+import App from "./containers/App";
+import {Provider} from "react-redux";
+import configureStore from "./store/configureStore";
+import rootSaga from "./sagas";
 
 const store = configureStore();
 store.runSaga(rootSaga);
--- client/src/games/store/configureStore.js
+++ client/src/games/store/configureStore.js
@@ -3,11 +3,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
 import createSagaMiddleware from 'redux-saga'
+import { createLogger } from 'redux-logger'
 
 export default function configureStore() {
-  const sagaMiddleware = createSagaMiddleware()
+  const sagaMiddleware = createSagaMiddleware();
+  const logger = createLogger();
   return {
-    ...createStore(rootReducer, applyMiddleware(sagaMiddleware)),
+    ...createStore(rootReducer, applyMiddleware(sagaMiddleware, logger)),
     runSaga: sagaMiddleware.run
   }
 }
